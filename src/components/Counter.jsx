f0ecba42a9b3901b987bc3165f7cb9d8189df5dd
MD. Mehedi Hasan Talha <mdmehedihasantalha2006@gmail.com>
2025-05-21 10:41:14 +0600

    connect function implemented by hook

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,21 +1,24 @@
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "../redux/counter/actions";
 
-function Counter({ count, increment, decrement }) {
-  return <button onClick={increment} onContextMenu={decrement}>count is {count}</button>;
+export default function Counter() {
+  const count = useSelector((state) => state.value);
+  const dispatch = useDispatch();
+
+  const incrementHandler = (value) => {
+    dispatch(increment(value));
+  };
+
+  const decrementHandler = (value) => {
+    dispatch(decrement(value));
+  };
+
+  return (
+    <button
+      onClick={() => incrementHandler(5)}
+      onContextMenu={() => decrementHandler(3)}
+    >
+      count is {count}
+    </button>
+  );
 }
-
-const mapStateToProps = (state) => {
-  return {
-    count: state.value,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: (value) => dispatch(increment(value)),
-    decrement: (value) => dispatch(decrement(value)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/redux/counter/counterReducer.js
+++ src/redux/counter/counterReducer.js
@@ -1,23 +1,22 @@
 import { DECREMENT, INCREMENT } from "./actionsType";
 
 const initialState = {
-    value: 0
-}
-
+  value: 0,
+};
 
 export default function counterReducer(state = initialState, action) {
-    switch (action.type) {
-        case INCREMENT:
-            return {
-                ...state,
-                value: state.value + 1
-            }
-        case DECREMENT:
-            return {
-                ...state,
-                value: state.value - 1
-            }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        value: state.value + action.payload,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        value: state.value - action.payload,
+      };
+    default:
+      return state;
+  }
 }
